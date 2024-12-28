353425615ff74b87005463ef3cd34b8939d030c6
Pacman <rulomania2011@hotmail.com>
2024-12-28 13:46:07 -0600

    firts commit

---
+++ src/test/java/com/free/WalletWise/WalletWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.free.WalletWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
