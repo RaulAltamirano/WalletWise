60b4ae0d762220ab5f43fea584cdb713c8d2d266
Pacman <rulomania2011@hotmail.com>
2025-01-02 13:22:11 -0600

    create(serviceConfig) create service to security

---
+++ src/main/java/infrastructure/configuration/SecurityConfig.java
@@ -0,0 +1,19 @@
+package infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
+
