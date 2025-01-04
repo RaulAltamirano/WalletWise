22bcf3b77da6e16a3bf9a220a16d4941c3308525
Pacman <rulomania2011@hotmail.com>
2025-01-03 22:27:14 -0600

    create(appUser) create new entity

---
+++ src/main/java/infrastructure/entity/AppUser.java
@@ -0,0 +1,23 @@
+package infrastructure.entity;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
+
+    // Getters y setters
+}
