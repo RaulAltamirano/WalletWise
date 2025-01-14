65bb8818a3689d8c005ca06c141e51d49cc32a24
Pacman <rulomania2011@hotmail.com>
2025-01-13 20:22:28 -0600

    create(apiResponse) create new class to handler response to api

---
+++ src/main/java/com/free/WalletWise/interfaces/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.free.WalletWise.interfaces;
+
+import java.time.LocalDateTime;
+
+public class ApiResponse<T> {
+    private final boolean success;
+    private final String message;
+    private final T data;
+    private final LocalDateTime timestamp;
+
+    private ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+
+}
